b2dc00cf43b3307172db4b6898c7baee9ee5da1b
Mohammed Abrar Ul Haq <mohammedabrar312@gmail.com>
2025-05-26 14:49:59 +0530

    Example - Flutter project is created, textfield field is created

---
+++ lib/src/app_form_textfield.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AppFormTextfield extends StatefulWidget {
+  const AppFormTextfield({super.key});
+
+  @override
+  State<AppFormTextfield> createState() => _AppFormTextfieldState();
+}
+
+class _AppFormTextfieldState extends State<AppFormTextfield> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
