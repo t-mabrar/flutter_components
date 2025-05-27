beaa1b739b0dc9559173de5a8bb9b0093315344c
Mohammed Abrar Ul Haq <mohammedabrar312@gmail.com>
2025-05-27 10:11:41 +0530

    Missing app_imports.dart file update is fixed

--- lib/src/core/app_imports.dart
+++ lib/src/core/app_imports.dart
@@ -1,6 +1,6 @@
 export 'app_logger.dart';
 export 'app_shared_preferences.dart';
 export 'app_utils.dart';
-export 'extensions.dart';
+export 'app_extensions.dart';
 export 'app_validations.dart';
 export 'app_enums.dart';
