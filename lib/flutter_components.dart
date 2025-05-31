8306a1a21bcee173c9e53300778d3000f2779024
Mohammed Abrar Ul Haq <mohammedabrar312@gmail.com>
2025-05-31 22:55:14 +0530

    Used export method rather than import

--- lib/flutter_components.dart
+++ lib/flutter_components.dart
@@ -5,3 +5,11 @@ export 'src/widgets/app_form_textfield.dart';
 export 'src/widgets/app_button.dart';
 export 'src/widgets/app_bottom_sheet.dart';
 export 'src/widgets/app_loading.dart';
+
+export 'package:provider/provider.dart';
+export 'package:sizer/sizer.dart';
+export 'package:responsive_framework/responsive_framework.dart';
+export 'package:dio/dio.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:get_it/get_it.dart';
+export 'package:injectable/injectable.dart';
--- lib/src/core/app_imports.dart
+++ lib/src/core/app_imports.dart
@@ -6,11 +6,3 @@ export 'app_utils.dart';
 export 'app_extensions.dart';
 export 'app_validations.dart';
 export 'app_enums.dart';
-
-import 'package:provider/provider.dart';
-import 'package:sizer/sizer.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-import 'package:dio/dio.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
